667e825b14be869b940b975bdb0834fe5c49463b
juaniboni <juanignacioboni@gmail.com>
2024-01-23 17:58:10 -0300

    Ordenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import "./App.css";
 import ShowProducts from "./components/ShowProducts";
+import ShowOrders from "./components/ShowOrders";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/admin/products" element={<ShowProducts />} />
+        <Route path="/orders" element={<ShowOrders />} />
       </Routes>
     </>
   );
---
+++ src/components/ShowOrders.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ShowOrders = () => {
+  const url = "http://localhost:3000/orders";
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const getOrders = async () => {
+    try {
+      const response = await axios.get(url);
+      setOrders(response.data);
+    } catch (error) {
+      console.error("Error al obtener órdenes:", error);
+      // Maneja el error según tus necesidades
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <div className="row mt-3">
+          <div className="col-12">
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>ID de Orden</th>
+                    <th>Estado</th>
+                    <th>Método de Pago</th>
+                    {/* Agrega más encabezados según tus necesidades */}
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order, index) => (
+                    <tr key={order.id}>
+                      <td>{index + 1}</td>
+                      <td>{order.id}</td>
+                      <td>{order.state}</td>
+                      <td>{order.payment_method}</td>
+                      {/* Agrega más celdas según tus necesidades */}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowOrders;
