869090cad07921363cf2db29ee3510eb3a4eeb8b
juaniboni <juanignacioboni@gmail.com>
2024-01-31 19:12:23 -0300

    Cambio de rutas

--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -5,7 +5,7 @@ import withReactContent from "sweetalert2-react-content";
 import { show_alerta } from "../functions";
 
 const ShowProducts = () => {
-  const url = "http://localhost:3000/admin/products";
+  const url = "http://localhost:3000/products";
   const [products, setProducts] = useState([]);
   const [id, setId] = useState("");
   const [name, setName] = useState("");
