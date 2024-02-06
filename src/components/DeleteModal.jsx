e062f0360e76414df24ad7a247a0ceadfe1b99a4
juaniboni <juanignacioboni@gmail.com>
2024-02-05 22:07:12 -0300

    Botones completos con modales

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,29 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-const DeleteModal = ({ isOpen, onClose, onDelete }) => {
+function DeleteModal({ onDelete }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = () => {
+    onDelete(); // Invoca la función onDelete pasada como prop para realizar la eliminación del producto
+    handleClose(); // Cierra el modal después de realizar la eliminación
+  };
+
   return (
-    <div className={`modal ${isOpen ? "is-active" : ""}`}>
-      <div className="modal-background" onClick={onClose}></div>
-      <div className="modal-content">
-        <div className="box">
-          <p>¿Estás seguro que deseas eliminar este producto?</p>
-        </div>
-      </div>
-      <button
-        className="modal-close is-large"
-        aria-label="close"
-        onClick={onClose}
-      ></button>
-      <footer className="modal-card-foot">
-        <button className="button is-danger" onClick={onDelete}>
-          Sí
-        </button>
-        <button className="button" onClick={onClose}>
-          Cancelar
-        </button>
-      </footer>
-    </div>
+    <>
+      <button variant="danger" onClick={handleShow}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+          />
+        </svg>
+      </button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Eliminar producto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ¿Estás seguro que deseas eliminar este producto?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Eliminar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
-};
+}
 
 export default DeleteModal;
